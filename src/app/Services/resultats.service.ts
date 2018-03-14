796b8b7674e43415b16250ff8c575840367cc15b
Jean Baptiste BOUILLET <jeanbaptiste.bouillet@startpeople.fr>
2018-03-14 09:29:06 +0100

    chargement des données depuis spreadsheet publique

--- src/app/Services/resultats.service.ts
+++ src/app/Services/resultats.service.ts
@@ -4,6 +4,7 @@ import {Observable} from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import { catchError, map, tap } from 'rxjs/operators';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import * as _ from 'underscore';
 
 @Injectable()
 export class ResultatsService {
@@ -11,12 +12,36 @@ export class ResultatsService {
   constructor( private http: HttpClient) {
 
   }
-
+  parseRow(row:Object):ResultatModel{
+    var resultat=new ResultatModel();
+    resultat.NomJoueur=row['gsx$joueur']['$t'];
+    resultat.Club=row['gsx$club']['$t'];
+    resultat.Licence=row['gsx$licence']['$t'];
+    resultat.Points=parseInt(row['gsx$points']['$t']);
+    resultat.Tournoi=row['gsx$tournoi']['$t'];
+    return resultat;
+  };
    getResultats(): Observable<ResultatModel[]> {
-      return this.http.get<ResultatModel[]>('/assets/data/resultats.json')
-      .pipe(
-        catchError(this.handleError('getResultats', []))
-      );
+
+
+
+var resultatsFromSheet=this.http.get('https://spreadsheets.google.com/feeds/list/15ytgQiUCdIgBDverpk9o3s786va4DREBPuZweCpyUjM/od6/public/values?alt=json')
+.pipe(
+  map((rows,idx)=>{
+    var entries=rows['feed']['entry'];
+    var items=_.map(entries,e=>this.parseRow(e));
+   return items;
+  })
+);
+
+return resultatsFromSheet;
+
+
+
+      // return this.http.get<ResultatModel[]>('/assets/data/resultats.json')
+      // .pipe(
+      //   catchError(this.handleError('getResultats', []))
+      // );
   }
 
 /**
