a6dc7babce11372b244523c201474136c4ca13a7
Jean-Baptiste Bouillet <jbval2@gmail.com>
2018-03-14 16:44:39 +0100

    cleanup

--- src/app/Components/Resultats/resultats.component.ts
+++ src/app/Components/Resultats/resultats.component.ts
@@ -12,29 +12,39 @@ import { ResultatsService } from '../../Services/resultats.service';
 
 export class ResultatsComponent implements OnInit {
   resultatsFiltered: ResultatModel[];
-  tournois: string[];
+  tournois: Set<string>;
   selectedTournoi:string;
   private resultats: ResultatModel[];
 
+  /**
+   * Crée une nouvelle instance de resultats.component
+   * @param resultatsServiceDependency Service de gestion des résultats
+   */
   constructor(private resultatsServiceDependency: ResultatsService) {
 
   }
-
+/**
+ * Initialisation du composant
+ */
   ngOnInit():void {
-    this.fetchResultats();
+    this.initResultats();
   }
-
-  private fetchResultats():void {
+/**
+ * Récupère l'ensemble des résultats tous tournois confondus
+ */
+  private initResultats():void {
     this.resultatsServiceDependency.getResultats().subscribe(resultat => {
       this.resultats = resultat;
-      this.tournois = _.union(
-        ['TOUS'],
-        _.uniq(_.map(this.resultats, r => r.Tournoi))
-      );
+      
+      this.tournois =new Set(['TOUS'].concat(this.resultats.map( r => r.Tournoi)));
       this.refreshResultats('TOUS');
     });
   }
-
+  
+/**
+ * Rafraichi la liste des résultats en fonction du tournoi courant
+ * @param selectedTournoi tournoi sélectionné
+ */
   public refreshResultats(selectedTournoi: string): void {
     this.selectedTournoi=selectedTournoi;
     if (selectedTournoi === 'TOUS') {
@@ -47,27 +57,32 @@ export class ResultatsComponent implements OnInit {
     const joueursGroupes = _.groupBy(this.resultatsFiltered, 'Licence');
     this.resultatsFiltered = [];
     _.each(joueursGroupes, (joueursParNom, numLicence) => {
-      const currentJoueur = joueursParNom[0];
-      const pointsTotalJoueur = joueursParNom.map(j => j.Points);
-      const sumJoueur = _.reduce(
-          pointsTotalJoueur,
-          (memo: number, joueur: number) => {
-            return memo + joueur;
-          }
-      );
-      const joueurConsolide: ResultatModel = {
-          NomJoueur: currentJoueur.NomJoueur,
-          Points: sumJoueur,
-          Club: currentJoueur.Club,
-          Tournoi: selectedTournoi,
-          Licence: currentJoueur.Licence
-      };
-      this.resultatsFiltered.push(joueurConsolide);
+      this.fillResultatsFiltre(joueursParNom,numLicence,selectedTournoi);
     });
 
+    
     this.resultatsFiltered = _.sortBy(
       this.resultatsFiltered,
       r => r.Points
     ).reverse();
   }
+
+  private fillResultatsFiltre(joueursParNom:ResultatModel[],numLicence:string,selectedTournoi:string):void{
+    const currentJoueur:ResultatModel = joueursParNom[0];
+    const pointsTotalJoueur = joueursParNom.map(j => j.Points);
+    const sumJoueur = pointsTotalJoueur.reduce(
+                        (memo: number, joueur: number) => {
+                          return memo + joueur;
+                        }
+                      );
+    const joueurConsolide: ResultatModel = {
+        NomJoueur: currentJoueur.NomJoueur,
+        Points: sumJoueur,
+        Club: currentJoueur.Club,
+        Tournoi: selectedTournoi,
+        Licence: currentJoueur.Licence
+    };
+    this.resultatsFiltered.push(joueurConsolide);
+}
+
 }
--- src/app/Services/resultats.service.ts
+++ src/app/Services/resultats.service.ts
@@ -4,10 +4,10 @@ import {Observable} from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import { catchError, map, tap } from 'rxjs/operators';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import * as _ from 'underscore';
 
 @Injectable()
 export class ResultatsService {
+  private urlDocGoogle:string='https://spreadsheets.google.com/feeds/list/15ytgQiUCdIgBDverpk9o3s786va4DREBPuZweCpyUjM/od6/public/values?alt=json';
 
   constructor( private http: HttpClient) {
 
@@ -21,27 +21,17 @@ export class ResultatsService {
     resultat.Tournoi=row['gsx$tournoi']['$t'];
     return resultat;
   };
-   getResultats(): Observable<ResultatModel[]> {
 
-
-
-var resultatsFromSheet=this.http.get('https://spreadsheets.google.com/feeds/list/15ytgQiUCdIgBDverpk9o3s786va4DREBPuZweCpyUjM/od6/public/values?alt=json')
-.pipe(
-  map((rows,idx)=>{
-    var entries=rows['feed']['entry'];
-    var items=_.map(entries,e=>this.parseRow(e));
-   return items;
-  })
-);
-
-return resultatsFromSheet;
-
-
-
-      // return this.http.get<ResultatModel[]>('/assets/data/resultats.json')
-      // .pipe(
-      //   catchError(this.handleError('getResultats', []))
-      // );
+  getResultats(): Observable<ResultatModel[]> {
+    var resultatsFromSheet=this.http.get(this.urlDocGoogle)
+    .pipe(
+      map((rows,idx)=>{
+        var entries=rows['feed']['entry'];
+        var items=entries.map(e=>this.parseRow(e));
+        return items;
+      })
+    );
+    return resultatsFromSheet;
   }
 
 /**
